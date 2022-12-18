1e5d954aa333ba8923522c2fff3302f85d566b3e
Hiago Macedo <hiago.m.a.macedo@gmail.com>
2022-12-17 21:34:45 -0300

    hidden key

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1+v^#p-l3@n=csldxa_@q%&(s=71eo*x$=)q*rd&@e)!+$0uto'
+# SECRET_KEY = 'development key'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
