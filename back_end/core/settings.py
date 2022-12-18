81773cb516360dedbf23e2de70b1bec1443ae4a2
Hiago Macedo <hiago.m.a.macedo@gmail.com>
2022-12-17 21:43:33 -0300

    Added python decouple

--- back_end/core/settings.py
+++ back_end/core/settings.py
@@ -18,9 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+from decouple import config
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'development key'
+SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
